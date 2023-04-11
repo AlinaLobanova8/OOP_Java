1a2daec2c85ff4fa512908907c055b6ca546737a
Alina <alobanova8888@gmail.com>
2023-04-11 13:34:08 +0300

    Homework 2

---
+++ lesson_2/Operation.java
@@ -0,0 +1,8 @@
+package OOP_Java.lesson_2;
+
+public interface Operation {
+    public double summa();
+    public double difference();
+    public double multiply();
+    public double devide();
+}
