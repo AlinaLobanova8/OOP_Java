7b97b834cde89a0e9247915e039b33362e1ab1cd
Alina <alobanova8888@gmail.com>
2023-04-24 22:46:32 +0300

    Homework 5

---
+++ lesson_5/service/UserBalance.java
@@ -0,0 +1,11 @@
+package OOP_Java.lesson_5.service;
+
+import OOP_Java.lesson_5.model.User;
+
+public class UserBalance {
+    void creditBalance(int index, int money);
+
+    int debitBalance(int index, int money);
+
+    int getBalance(int index);
+}
